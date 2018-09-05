bd545d2004433683c8bfe0e36e68aa6d4485c990
Danila <daniladashko@gmail.com>
2018-09-05 23:47:51 +0600

    Add remote akka configurations. Add class for running server.

--- src/main/java/kz/desh/snowballs/SnowballsApplication.java
+++ src/main/java/kz/desh/snowballs/SnowballsApplication.java
@@ -1,6 +1,33 @@
 package kz.desh.snowballs;
 
+import akka.actor.ActorSystem;
+import lombok.val;
+
+import java.util.Scanner;
+
 public class SnowballsApplication {
+    private static final String STOP_SERVER_COMMAND = "stop";
+
     public static void main(String[] args) {
+        val system = ActorSystem.create("snowballs");
+        listenConsoleCommands(system);
+    }
+
+    private static void listenConsoleCommands(ActorSystem system) {
+        System.out.print("Command: ");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            String inputString = scanner.nextLine();
+            if (!STOP_SERVER_COMMAND.equals(inputString)) {
+                listenConsoleCommands(system);
+            } else {
+                stopServer(system);
+            }
+        }
+    }
+
+    private static void stopServer(ActorSystem system) {
+        system.terminate();
+        System.exit(0);
     }
 }
