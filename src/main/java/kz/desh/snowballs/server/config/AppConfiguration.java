eb4d2604117cee69449846c234f6a970f147680f
Danila <daniladashko@gmail.com>
2018-09-08 23:50:03 +0600

    Remove component scan

--- src/main/java/kz/desh/snowballs/server/config/AppConfiguration.java
+++ src/main/java/kz/desh/snowballs/server/config/AppConfiguration.java
@@ -4,22 +4,16 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import kz.desh.snowballs.server.LoginActor;
 import kz.desh.snowballs.server.ServerActor;
-import kz.desh.snowballs.server.commands.LoginCommand;
 import kz.desh.snowballs.server.commands.executor.CommandExecutor;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 import static kz.desh.snowballs.server.config.SpringExtension.SPRING_EXTENSION_PROVIDER;
 
 @Configuration
-@ComponentScan("kz.desh.snowballs")
 public class AppConfiguration {
     @Autowired
     private ApplicationContext applicationContext;
