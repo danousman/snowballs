8274675453a84d490bae23777eefa1f2319425bc
Danila <daniladashko@gmail.com>
2018-09-23 19:59:08 +0600

    Add level cost and study time.

--- src/main/java/kz/desh/snowballs/server/entity/AbilityType.java
+++ src/main/java/kz/desh/snowballs/server/entity/AbilityType.java
@@ -1,12 +1,19 @@
 package kz.desh.snowballs.server.entity;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum AbilityType {
-    BIG_SNOWBALL(new AbilityEntity("Большой снежок", "Big snowball", 5, 15, 0, 0, 0));
+    BIG_SNOWBALL(new AbilityEntity("Большой снежок", "Big snowball", 5, 15, 0, 0, 0), 10, 600);
 
     private final AbilityEntity ability;
+
+    @Getter
+    private final int levelCost;
+
+    @Getter
+    private final int studyTime;
 
     public AbilityEntity getAbility() {
         this.ability.setType(this);
